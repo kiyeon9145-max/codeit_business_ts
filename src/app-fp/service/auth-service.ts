452dc273e3a7cf34c9994b294d74da75ab59db89
kiyeon9145-max <kiyeon9145@gmail.com>
2026-06-19 14:20:33 +0900

    fix

--- src/app-fp/service/auth-service.ts
+++ src/app-fp/service/auth-service.ts
@@ -6,16 +6,20 @@ export type UserDataType = {
   credential: string;
 };
 
+// 순수함수 + 불변성
 export const authService = (userRepo: IUserRepo) => {
   const { findUserByEmail, saveUser } = userRepo;
 
+  // 순수함수 + 불변성
   const createCredential = (email: string, password: string) => {
     return `${email}-${password}`;
   };
+  // 비순수함수(부수효과를 격리) + 불변함수
   const signIn = (
     inputEmail: string,
     inputPassword: string,
   ): UserDataType | null => {
+    // 부수효과(격리)
     const foundUser = findUserByEmail(inputEmail);
 
     if (foundUser === null) {
@@ -32,7 +36,21 @@ export const authService = (userRepo: IUserRepo) => {
       credential: createCredential(foundUser.email, foundUser.password),
     };
   };
-  const signUp = () => {};
+  // 비순수함수(부수효과를 격리) + 불변성
+  const signUp = (
+    inputEmail: string,
+    inputPassword: string,
+    inputUsername: string,
+  ): boolean => {
+    const foundUser = findUserByEmail(inputEmail);
+
+    if (foundUser !== null) {
+      return false;
+    }
+
+   saveUser(inputEmail, inputPassword, inputUsername);
+    return true;
+  };
 
   return { signIn, signUp };
 };
---
+++ src/app-fp/service/memo-service.ts
@@ -0,0 +1,52 @@
+import { userDomainType } from "../domain/user";
+import { IMemoRepo } from "./ports/memo-repo.interface";
+import { IUserRepo } from "./ports/user-repo.interface";
+
+type MemoDataType = { email: string; title: string; content: string };
+
+export const memoService = (
+  userDomain: userDomainType,
+  userRepo: IUserRepo,
+  memoRepo: IMemoRepo,
+) => {
+  const { isCredentialValidate } = userDomain;
+  const { findUserByEmail } = userRepo;
+  const { loadMemos, saveMemo } = memoRepo;
+
+  const getMyMemos = (credential: string): MemoDataType[] => {
+    const email = credential.split("-")[0];
+    const foundUser = findUserByEmail(email);
+
+    if (foundUser === null) {
+      return [];
+    }
+    if (isCredentialValidate(foundUser, credential) === false) {
+      return [];
+    }
+
+    const memos = loadMemos();
+    return memos
+      .filter((memo) => memo.email === email)
+      .map((memo) => {
+        return { email: memo.email, title: memo.title, content: memo.content };
+      });
+  };
+  const createMemo = (credential: string, title: string, content: string) => {
+    const email = credential.split("-")[0];
+    const foundUser = findUserByEmail(email);
+
+    if (foundUser === null) {
+      return false;
+    }
+    if (isCredentialValidate(foundUser, credential) === false) {
+      return false;
+    }
+
+    saveMemo(email, title, content);
+    return true;
+  };
+
+  return { getMyMemos, createMemo };
+};
+
+export type MemoServiceType = ReturnType<typeof memoService>;
